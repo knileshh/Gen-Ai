79b810fc4cf6273c5fc43a99f081c9da51878f1b
knilesh9 <neutron_stays@protonmail.com>
2024-03-11 23:17:01 +0530

    Working propt hard coded

---
+++ Gen Ai/src/backend/index.js
@@ -0,0 +1,23 @@
+import 'dotenv/config'
+
+
+// const { GoogleGenerativeAI } = require("@google/generative-ai");
+import { GoogleGenerativeAI } from '@google/generative-ai';
+
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API);
+
+async function run() {
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+  const prompt = "Write a line."
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
