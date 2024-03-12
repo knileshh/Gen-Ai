b00d5a854038c9886ae9f15caca28f475062c62a
knilesh9 <neutron_stays@protonmail.com>
2024-03-13 00:41:31 +0530

    Working GEMINI Worker fn()

--- Serverless fn/my-app/src/index.ts
+++ Serverless fn/my-app/src/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import { env, getRuntimeKey } from 'hono/adapter'
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
 const app = new Hono();
+
+app.use(cors())
 
 async function run(content: any) {
   // For text-only input, use the gemini-pro model
@@ -20,14 +23,18 @@ async function run(content: any) {
 
 app.get('/', async (c) => {
 
-  // const {GEMINI_API} = env(c)
   const ans = await run(c);
-  console.log(ans)
-  console.log("my key:")
   return c.json({
       message: 'Live!',
       Quote: ans
   })
+})
+
+app.get('/v1', async (c) => {
+
+  const ans = await run(c);
+  console.log(ans)
+  return c.text(ans)
 })
 
 
